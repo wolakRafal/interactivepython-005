e4cb4f20cc07b2ea6ab9ee5e36cb491ae6db5c81
Rafał Wolak <RWolak@advaoptical.com>
2014-11-01 23:42:09 +0100

    Initial commit

---
+++ miniprojects/Spaceship.py
@@ -0,0 +1,76 @@
+__author__ = 'RafalW'
+# http://www.codeskulptor.org/#user38_wzIPzdjD6i_2.py
+
+# Partial example code for Spaceship
+
+import simplegui, math
+
+# GLOBAL CONST
+FRICTION = 0.07
+
+class ImageInfo:
+    def __init__(self, center, size, radius = 0, lifespan = None, animated = False):
+        self.center = center
+        self.size = size
+        self.radius = radius
+        if lifespan:
+            self.lifespan = lifespan
+        else:
+            self.lifespan = float('inf')
+        self.animated = animated
+
+    def get_center(self):
+        return self.center
+
+    def get_size(self):
+        return self.size
+
+    def get_radius(self):
+        return self.radius
+
+    def get_lifespan(self):
+        return self.lifespan
+
+    def get_animated(self):
+        return self.animated
+
+
+# art assets created by Kim Lathrop, may be freely re-used in non-commercial projects, please credit Kim
+
+# ship image
+ship_info = ImageInfo([45, 45], [90, 90], 35)
+ship_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/double_ship.png")
+
+# sound assets purchased from sounddogs.com, please do not redistribute
+ship_thrust_sound = simplegui.load_sound("http://commondatastorage.googleapis.com/codeskulptor-assets/sounddogs/thrust.mp3")
+
+
+# Ship class
+class Ship:
+    def __init__(self, pos, vel, angle, image, info):
+        self.pos = [pos[0],pos[1]]
+        self.vel = [vel[0],vel[1]]
+        self.thrust = False
+        self.angle = angle
+        self.angle_vel = 0
+        self.image = image
+        self.image_center = info.get_center()
+        self.image_size = info.get_size()
+        self.radius = info.get_radius()
+
+    def draw(self,canvas):
+        #canvas.draw_circle(self.pos, self.radius, 1, "White", "White")
+        canvas.draw_image(self.image, self.image_center, self.image_size, self.pos, self.image_size, self.angle)
+
+    def update(self):
+        # Position Update
+        self.pos[0] += self.vel[0]
+        self.pos[1] += self.vel[1]
+        # Friction update
+        self.vel[0] *= (1 - FRICTION)
+        self.vel[1] *= (1 - FRICTION)
+        # Velocity Update - acceleration in direction of forward vestor
+        if self.thrust:
+            forward = [math.cos(self.angle), math.sin(self.angle)]
+            self.vel[0] += forward[0]
+            self.vel[1] += forward[1]
